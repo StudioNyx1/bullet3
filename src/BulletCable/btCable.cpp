02afa0c0c6d8c7a673ed9a1e90c23c67910187e1
Benjamin Holmes <benjamin.holmes@studio-nyx.com>
2025-12-17 10:50:04 +0100

    ✨🔨[Cable] - ST-3 - Use the tense accumulator to calculate an impulse in AnchorConstraint's methods
     - Swap out the node's placemenet and the distance calculated between an anchor and his attached node

--- src/BulletCable/btCable.cpp
+++ src/BulletCable/btCable.cpp
@@ -1152,8 +1152,21 @@ void btCable::anchorConstraint()
 		const btVector3 va = anchor.m_body->getVelocityInLocalPoint(anchor.m_c1) * dt;
 		const btVector3 vb = node.m_x - node.m_q_sub;
 		const btVector3 vr = (va - vb) + (wa - node.m_x) * kAHR;
-		btVector3 impulse = anchor.m_c0_massBalance * vr;
+		btVector3 impulseBullet = anchor.m_c0 * vr;
+		btVector3 impulseMassBalance = anchor.m_c0_massBalance * vr;
 
+		btScalar ratio = 0.0;
+		if (m_tenseAccumulator > m_minAccumulator)
+		{
+			btScalar x = btClamped((m_tenseAccumulator - m_minAccumulator) / (m_maxAccumulator - m_minAccumulator), 0.0, 1.0);
+			// ratio = m_tenseAccumulator;
+			ratio = x;
+			// ratio = 1.0 - btPow(1.0 - m_tenseAccumulator, 4.0);
+			// ratio = btPow(x, 4.0);
+		}
+		btVector3 impulse = lerp(impulseBullet, impulseMassBalance, ratio);
+
+		// Clamp the calculated impulse
 		btScalar currentTension = anchor.m_lastTension.length();
 		anchor.m_lastTension += impulse / dt;
 		btScalar finalTension = anchor.m_lastTension.length();
@@ -1167,8 +1180,8 @@ void btCable::anchorConstraint()
 		anchor.m_totalTension += impulse / dt;
 
 		// Update anchor's data
+		node.m_x += impulseMassBalance * anchor.m_c2_massBalance; // lerp(impulseBullet * anchor.m_c2, impulseMassBalance * anchor.m_c2_massBalance, ratio);
 		anchor.m_dist = wa.distance(node.m_x);
-		node.m_x += impulse * anchor.m_c2_massBalance;
 		anchor.m_body->applyImpulse(-impulse, anchor.m_c1);
 	}
 }
@@ -1189,11 +1202,22 @@ void btCable::anchorConstraintPlacement()
 		Node& node = *anchor.m_node;
 
 		const btVector3 wa = body.getWorldTransform() * anchor.m_local;
-		const btVector3 va = anchor.m_body->getVelocityInLocalPoint(anchor.m_c1) * dt;
-		const btVector3 vb = node.m_x - node.m_q_sub;
-		const btVector3 vr = (va - vb) + (wa - node.m_x) * kAHR;
-		btVector3 impulse = anchor.m_c0_massBalance * vr;
+		const btVector3 vr = (wa - node.m_x) * kAHR;
+		btVector3 impulseBullet = anchor.m_c0 * vr;
+		btVector3 impulseMassBalance = anchor.m_c0_massBalance * vr;
 
+		btScalar ratio = 0.0;
+		if (m_tenseAccumulator > m_minAccumulator)
+		{
+			btScalar x = btClamped((m_tenseAccumulator - m_minAccumulator) / (m_maxAccumulator - m_minAccumulator), 0.0, 1.0);
+			// ratio = m_tenseAccumulator;
+			ratio = x;
+			// ratio = 1.0 - btPow(1.0 - m_tenseAccumulator, 4.0);
+			// ratio = btPow(x, 4.0);
+		}
+		btVector3 impulse = lerp(impulseBullet, impulseMassBalance, ratio);
+
+		// Clamp the calculated impulse
 		btScalar currentTension = anchor.m_lastTension.length();
 		anchor.m_lastTension += impulse / dt;
 		btScalar finalTension = anchor.m_lastTension.length();
@@ -1207,8 +1231,8 @@ void btCable::anchorConstraintPlacement()
 		anchor.m_totalTension += impulse / dt;
 
 		// Update anchor's data
+		node.m_x += impulseMassBalance * anchor.m_c2_massBalance; // lerp(impulseBullet * anchor.m_c2, impulseMassBalance * anchor.m_c2_massBalance, ratio);
 		anchor.m_dist = wa.distance(node.m_x);
-		node.m_x = wa;
 		anchor.m_body->applyImpulse(-impulse, anchor.m_c1);
 	}
 }
